c18c134e54ed3991ea8a0fb34016bc2b7f441d49
HaeChan <ckssmsla98@naver.com>
2021-05-12 04:15:29 +0900

    text mode TodoList complete

--- python3/sql/Todosql.py
+++ python3/sql/Todosql.py
@@ -4,25 +4,30 @@ conn = pymysql.connect(host='localhost', user='root', password='1234',db='TodoDB
 
 cursor = conn.cursor()
 
-#select
-
+#조회
 def select():
+    print("( ID | contents | Did or yet )")
     cursor.execute("SELECT * FROM dataTBL")
     res = cursor.fetchall()
     for data in res:
          print(data)
 
+#투두 아이템 삽입
 def insert(index,contents,do):
     cursor.execute("INSERT INTO dataTBL VALUES ({0},%s,{1})".format(index,do),contents)
 
+#아이템 삭제
 def delect(index):
     cursor.execute("DELETE FROM dataTBL WHERE ID = {0}".format(index))
 
+#이행한 상태 변화
+def status_change(index,boolean):
+    cursor.execute("UPDATE dataTBL SET Do = {0} WHERE ID = {1}".format(boolean,index))
 
 case = 0
-
-while case !=4:    
-    print("<1> SELECT , <2> INSERT(index,contents,do) , <3> DELETE(index), <4> EXIT")
+print("\n\n****** This is simple Todo List Text Application ******\n\n")
+while case !=5:    
+    print("<1> SELECT      <2> INSERT(index,contents,do)      <3> DELETE(index)\n<4> status_change( index, status(0-yet , 1-did))      <5> EXIT\n")
     case = int(input("input :"))
     if case==1:
         select()
@@ -34,6 +39,10 @@ while case !=4:
         de_index = input("index :")
         delect(de_index)
     elif case == 4:
+        ch_index = input("index :")
+        ch_bool = input("0 or 1 :")
+        status_change(ch_index,ch_bool)
+    elif case == 5:
         print("BYE")
     else:
         print("Error")
